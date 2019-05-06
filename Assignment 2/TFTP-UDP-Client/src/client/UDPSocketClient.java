4e2aa07c743354991f46149f175f4568fb8183c1
samuelwithey <samuelwithey@gmail.com>
2019-05-06 23:13:18 +0100

    All packets working with correct tesing

--- Assignment 2/TFTP-UDP-Client/src/client/UDPSocketClient.java
+++ Assignment 2/TFTP-UDP-Client/src/client/UDPSocketClient.java
@@ -1,16 +1,17 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.Byte.compare;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class UDPSocketClient {
-    
-    //java function to turn data into a byte array
     
     private final int OP_RRQ = 1;
     private final int OP_WRQ = 2;
@@ -21,14 +22,10 @@ public class UDPSocketClient {
     private InetAddress address;
     private DatagramSocket socket;
     private DatagramPacket packet;
-    private Scanner scanner = new Scanner(System.in);
     private FileOutputStream outputStream;
     private FileInputStream inputStream;
     
-    private UDPSocketClient() {
-        //set address
-        //set socket with random port and address
-        //set timeout
+    public UDPSocketClient() {
         getUserInput();
     }
     
@@ -36,86 +33,69 @@ public class UDPSocketClient {
         return packet.getLength() < 512; //needs rethinking
     }
     
-    private DatagramPacket generateReadandWrtiePacket(int opcode, String fileName, InetAddress address, int port) {
-        /**
-         * Need to think of a better way to do this method. It isn't very efficient
-         */
-        int totalBytes = 0;
+    public DatagramPacket generateReadandWrtiePacket(int opcode, String fileName, InetAddress address, int port) {
         byte[] buf = new byte[516];
-        buf[0] = (byte) opcode; //not right
-        buf[1] = (byte) opcode; //not right
-        totalBytes = 2;
-        byte[] file = fileName.getBytes();
-        totalBytes += file.length;
-        System.arraycopy(file, 0, buf, 2, file.length);
-        byte f = 0;
-        totalBytes += 1;
-        buf[totalBytes] = f;
-        byte[] mode = "octet".getBytes();
-        totalBytes += 1;
-        System.arraycopy(mode, 0, buf, totalBytes , mode.length);
-        totalBytes += mode.length;
-        totalBytes += 1;
-        buf[totalBytes] = f; //don't think this is correct
-        DatagramPacket pkt = new DatagramPacket(buf, totalBytes, address, port);
+        System.arraycopy(setOpcode(opcode), 0, buf, 0 , setOpcode(opcode).length);
+        System.arraycopy((fileName+"\0").getBytes(), 0, buf, setOpcode(opcode).length, fileName.length() + 1);
+        System.arraycopy(("octet"+"\0").getBytes(), 0, buf, (setOpcode(opcode).length + fileName.length()) + 1, ("octet"+"\0").length());
+        DatagramPacket pkt = new DatagramPacket(buf, buf.length, address, port);
         return pkt;
     }
     
-    private DatagramPacket generateDataPacket(int blockNum, byte[] data, InetAddress address, int port) {
+    public DatagramPacket generateDataPacket(int blockNum, byte[] data, InetAddress address, int port) {
         byte[] buf = new byte[516];
-        /**
-         * I don't think any of the casting is right at all.
-         */
-        buf[0] = (byte) OP_DATA;
-        buf[1] = (byte) OP_DATA;
-        buf[2] = (byte) blockNum;
-        buf[3] = (byte) blockNum;
-        System.arraycopy(data, 0, buf, 5, data.length);
+        System.arraycopy(setOpcode(OP_DATA), 0, buf, 0, setOpcode(OP_DATA).length);
+        System.arraycopy(setBlockNum(blockNum), 0, buf,setOpcode(OP_DATA).length, setBlockNum(blockNum).length);
+        System.arraycopy(data, 0, buf, (setOpcode(OP_DATA).length + setBlockNum(blockNum).length), data.length);
         DatagramPacket pkt = new DatagramPacket(buf, data.length + 4, address, port);
-        return null;
+        return pkt;
     }
     
-    private DatagramPacket generateAckPacket(int blockNum, InetAddress address, int port) {
+    public DatagramPacket generateAckPacket(int blockNum, InetAddress address, int port) {
         byte[] buf = new byte[516];
-        buf[0] = (byte) OP_ACK;
-        buf[1] = (byte) OP_ACK;
-        buf[2] = (byte) blockNum;
-        buf[3] = (byte) blockNum;
+        System.arraycopy(setOpcode(OP_ACK), 0, buf, 0, setOpcode(OP_ACK).length);
+        System.arraycopy(setBlockNum(blockNum), 0, buf, setOpcode(OP_ACK).length,  setBlockNum(blockNum).length);
         DatagramPacket pkt = new DatagramPacket(buf, 4, address, port);
         return pkt;
     }
     
-    private DatagramPacket generateErrorPacket(int errorCode, String errorMsg, InetAddress address, int port) {
+    public DatagramPacket generateErrorPacket(int errorCode, String errorMsg, InetAddress address, int port) {
         byte[] buf = new byte[516];
-        buf[0] = (byte) OP_ERROR;
-        buf[1] = (byte) OP_ERROR;
-        buf[2] = (byte) errorCode;
-        buf[3] = (byte) errorCode;
-        System.arraycopy(errorMsg.getBytes(), 0, buf, 4, errorMsg.getBytes().length);
-        buf[errorMsg.getBytes().length + 4] = (byte) 0;
-        DatagramPacket pkt = new DatagramPacket(buf, errorMsg.getBytes().length + 4, address, port);
+        byte[] errCode = setErrorCode(errorCode);
+        System.arraycopy(setOpcode(OP_ERROR), 0, buf, 0, setOpcode(OP_ERROR).length);
+        System.arraycopy(setErrorCode(errorCode), 0, buf, setOpcode(OP_ERROR).length, setErrorCode(errorCode).length);
+        System.arraycopy((errorMsg+"\0").getBytes(), 0, buf, setOpcode(OP_ERROR).length + setErrorCode(errorCode).length, errorMsg.getBytes().length + 1);
+        DatagramPacket pkt = new DatagramPacket(buf, buf.length, address, port);
         return pkt;
     }
     
     public void getUserInput() {
+        BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
         boolean finished = false;
-        while(finished) {
-            int userInput;
-            System.out.println("Press \"1\" if you would like to send a file to the server");
-            System.out.println("Press \"2\" if you would like to get a file from the server");
-            System.out.println("Press \"3\" at any time if you would like to exit the program");
-            System.out.println("Input: ");
-            userInput = scanner.nextInt();
-            if(userInput == 1) {
-                //get file name
-                //call method to send a file to the server
-            } else if(userInput == 2) {
-                //get file from users local system
-                //call method to get a file from the server
-            } else {
-                finished = true;
+        try {
+            while(finished) {
+                int userInput;
+                System.out.println("Press \"1\" if you would like to send a file to the server");
+                System.out.println("Press \"2\" if you would like to get a file from the server");
+                System.out.println("Press \"3\" if you would like to exit the program");
+                System.out.println("Input: ");
+                userInput = Integer.parseInt(input.readLine());
+                if(userInput == 1) {
+                    System.out.println("Please enter the filename to send to the server: ");
+                    String fileName = input.readLine();
+                    writeFile(fileName);
+                    finished = true; //could call the method again?
+                } else if(userInput == 2) {
+                    System.out.println("Please enter the filename of the file on the server");
+                    String fileName = input.readLine();
+                    receiveFile(fileName);
+                    finished = true; //could call the method again?
+                } else {
+                    finished = true;
+                }
             }
-            
+        } catch(IOException e) {
+            System.out.println("Error");
         }
     }
     
@@ -123,53 +103,121 @@ public class UDPSocketClient {
         //sends or receives the packet
     }
     
-    private void writeFile() {
+    private void writeFile(String fileName) {
+        //get file
+        //start sending packets
         //send packets of 512 until last packet is sent
     }
     
-    private void receiveFile() {
+    private void receiveFile(String fileName) {
+        //get file from server
+        //get the packet data
         //get a file and write it locally
     }
+    
+    public byte[] setOpcode(int opcode) {
+        byte[] arr = new byte[2];
+        arr[0] = (byte) ((opcode >> 8) & 0xFF);
+        arr[1] = (byte) (opcode & 0xFF);
+        return arr;
+    }
+    
+    public int getOpcode(byte[] arr) {
+        return((arr[0] & 0xFF) << 8) | (arr[1] & 0xFF);
+    }
+    
+    public byte[] setBlockNum(int blockNum) {
+        byte[] arr = new byte[2];
+        arr[0] = (byte) ((blockNum >> 8) & 0xFF);
+        arr[1] = (byte) (blockNum & 0xFF);
+        return arr;
+    }
+    
+    public int getBlockNum(byte[] arr) {
+        return((arr[2] & 0xFF) << 8) | (arr[3] & 0xFF);
+    }
+    
+    public byte[] setErrorCode(int errorCode) {
+        byte[] arr = new byte[2];
+        arr[0] = (byte) ((errorCode >> 8) & 0xFF);
+        arr[1] = (byte) (errorCode & 0xFF);
+        return arr;
+    }
+    
+    public int getErrorCode(byte[] arr) {
+        return((arr[2] & 0xFF) << 8) | (arr[3] & 0xFF);
+    }
+    
+    public String getFileName(byte[] file) {
+        String fileName = "";
+        byte zero = 0;
+        boolean found = false;
+        int i = 2;
+        while(!found && i < file.length) {
+            if(compare(file[i], zero) != 0){
+                fileName += (char) file[i];
+            } else {
+                found = true;
+            }
+            i++;
+        }
+        return fileName;
+    }
+    
+    public String getErrorMsg(byte[] errorMsg) {
+        byte[] errMsg = Arrays.copyOfRange(errorMsg, 4, errorMsg.length - 1);
+        return new String(errMsg);
+    }
+    
+    /**
+     * No need
+     * @param mode
+     * @return 
+     */
+//    public String getMode(byte[] mode) {
+//        return null;
+//    }
+            
 
     // the client will take the IP Address of the server (in dotted decimal format as an argument)
     // given that for this tutorial both the client and the server will run on the same machine, you can use the loopback address 127.0.0.1
     public static void main(String[] args) throws IOException {
-        
-        DatagramSocket socket;
-        DatagramPacket packet;
-        
-        
-        if (args.length != 1) {
-            System.out.println("the hostname of the server is required");
-            return;
-        }
-        
-        int len = 516;
-        byte[] buf = new byte[len];
-
-        /*
-        This is not the port set in the server. If the same port is selected, it will throw an exception because the server
-        is listening to this port and both processes run on the same machine.
-        */
-        socket = new DatagramSocket(4000); //Instantiating the DatagramSocket socket object
-
-        
-        // The address must be transfomed from a String to an InetAddress (an IP addresse object in Java).
-        InetAddress address = InetAddress.getByName(args[0]); //getting the address from args[0]
-
-        packet = new DatagramPacket(buf, len); //Instantiating a packet
-        packet.setAddress(address); //set IP address
-        packet.setPort(9000); //set port fields
-
-        // The server will respond to any kind of request (i.e. regardless of the packet payload)
-        socket.send(packet); //Send datagram packet to server (blocking call)
-
-        // DatagramPacket can be reused and values are overriden
-        socket.receive(packet); //Receive a packet containing the servers response
-
-        // display response
-        String received = new String(packet.getData());
-        socket.close();
+//        
+//        DatagramSocket socket;
+//        DatagramPacket packet;
+//        
+//        
+//        if (args.length != 1) {
+//            System.out.println("the hostname of the server is required");
+//            return;
+//        }
+//        
+//        int len = 516;
+//        byte[] buf = new byte[len];
+//
+//        /*
+//        This is not the port set in the server. If the same port is selected, it will throw an exception because the server
+//        is listening to this port and both processes run on the same machine.
+//        */
+//        socket = new DatagramSocket(4000); //Instantiating the DatagramSocket socket object
+//
+//        
+//        // The address must be transfomed from a String to an InetAddress (an IP addresse object in Java).
+//        InetAddress address = InetAddress.getByName(args[0]); //getting the address from args[0]
+//
+//        packet = new DatagramPacket(buf, len); //Instantiating a packet
+//        packet.setAddress(address); //set IP address
+//        packet.setPort(9000); //set port fields
+//
+//        // The server will respond to any kind of request (i.e. regardless of the packet payload)
+//        socket.send(packet); //Send datagram packet to server (blocking call)
+//
+//        // DatagramPacket can be reused and values are overriden
+//        socket.receive(packet); //Receive a packet containing the servers response
+//
+//        // display response
+//        String received = new String(packet.getData());
+//        socket.close();
     }
     
 }
---
+++ Assignment 2/TFTP-UDP-Client/test/client/PacketTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ *
+ * @author samue
+ */
+public class PacketTest {
+    
+    UDPSocketClient client;
+    private final int OP_RRQ = 1;
+    private final int OP_WRQ = 2;
+    private final int OP_DATA = 3;
+    private final int OP_ACK = 4;
+    private final int OP_ERROR = 5;
+    
+    @Before
+    public void setUp() {
+        client = new UDPSocketClient();
+    }
+
+    @Test
+    public void createReadRequest() throws IOException {
+        String fileName = "helloworld.txt";
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket pkt = client.generateReadandWrtiePacket(OP_RRQ, fileName, address, 2000);
+        System.out.println("Read Request Opcode: " + client.getOpcode(pkt.getData()));
+        assertThat(OP_RRQ, is(equalTo(client.getOpcode(pkt.getData()))));
+        System.out.println("Read Request filename: " + client.getFileName(pkt.getData()));
+        assertThat(fileName, is(equalTo(client.getFileName(pkt.getData()))));
+    }
+    
+    @Test
+    public void createWriteRequest() throws IOException{
+        String fileName = "helloworld.txt";
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket pkt = client.generateReadandWrtiePacket(OP_WRQ, fileName, address, 2000);
+        System.out.println("Write Request Opcode: " + client.getOpcode(pkt.getData()));
+        assertThat(OP_WRQ, is(equalTo(client.getOpcode(pkt.getData()))));
+        System.out.println("Write Request filename: " + client.getFileName(pkt.getData()));
+        assertThat(fileName, is(equalTo(client.getFileName(pkt.getData()))));
+    }
+    
+    @Test
+    public void createDataPacket() throws IOException {
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        //DatagramPacket pkt = client.generateDataPacket(OP_RRQ, data, address, OP_RRQ)
+    }
+    
+    @Test
+    public void createAckPacket() throws IOException{
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket pkt = client.generateAckPacket(0, address, 2000);
+        System.out.println("Ack packet opcode: " + client.getOpcode(pkt.getData()));
+        assertThat(OP_ACK, is(equalTo(client.getOpcode(pkt.getData()))));
+        System.out.println("Ack packet block #: " + client.getBlockNum(pkt.getData()));
+        assertThat(0, is(equalTo(client.getBlockNum(pkt.getData()))));
+    }
+    
+    @Test
+    public void createErrorPacket() throws IOException {
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        String e = "File not found !";
+        DatagramPacket pkt = client.generateErrorPacket(1, e, address, 2000);
+        System.out.println("Error packet opcode: " + client.getOpcode(pkt.getData()));
+        assertThat(OP_ERROR, is(equalTo(client.getOpcode(pkt.getData()))));
+        System.out.println("Error packet error code: " + client.getErrorCode(pkt.getData()));
+        assertThat(1, is(equalTo(client.getErrorCode(pkt.getData()))));
+        System.out.println("Error packet error message: " + client.getErrorMsg(pkt.getData()));
+        //assertThat(e, is(equalTo(client.getErrorMsg(pkt.getData()))));
+    }
+    
+}
